f7828142a47fdd6e2388588f8b4a549e25a9e40c
kimhyerin0909 <khyerin0909@gmail.com>
2022-10-04 13:46:22 +0900

    Feat - 대여자, 공급자 나눠서 채팅방 보여주기

--- src/Channels.js
+++ src/Channels.js
@@ -11,27 +11,52 @@ import db from "./firebaseConfig";
 
 export const Channels = () => {
   const [channels, setChannels] = useState([]);
+  const [ownerCh, setOwnerCh] = useState([]);
   const user_id = 4;
 
-  const getChannels = async () => {
+  const getLoanerChannels = async () => {
     const lq = query(
       collection(db, "messages"),
       where("loaner_id", "==", user_id)
     );
 
     const lunsub = onSnapshot(lq, (querySnap) => {
+      setChannels([]);
       querySnap.forEach((doc) => {
         setChannels((prev) => [...prev, doc.data()]);
       });
     });
   };
 
-  useMemo(() => getChannels(), []);
+  const getOwnerChannels = async () => {
+    const oq = query(
+      collection(db, "messages"),
+      where("owner_id", "==", user_id)
+    );
+
+    const ounsub = onSnapshot(oq, (querySnap) => {
+      setOwnerCh([]);
+      querySnap.forEach((doc) => {
+        setOwnerCh((prev) => [...prev, doc.data()]);
+      });
+    });
+  };
+
+  useMemo(() => getLoanerChannels(), []);
+  useMemo(() => getOwnerChannels(), []);
 
   return (
     <div>
+      <span>자신이 대여자</span>
       {channels &&
         channels.map((data) => (
+          <div key={data.id}>
+            <Link to={`/chat/${data.id.substr(1)}`}>{data.id}</Link>
+          </div>
+        ))}
+      <span>자신이 공급자</span>
+      {ownerCh &&
+        ownerCh.map((data) => (
           <div key={data.id}>
             <Link to={`/chat/${data.id.substr(1)}`}>{data.id}</Link>
           </div>
